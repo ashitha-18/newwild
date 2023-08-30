823a355d026c4db7576505d0bb66bb7b1819d6a3
ashitha-18 <ashi18pulari@gmail.com>
2023-08-30 13:25:00 +0530

    ethers import error corrected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Charities from './components/charities';
 import './App.css';
+import New from './components/SupportTokenWrapper';
 
 function App() {
   return (
     <div className="App">
       <h1>wildpatron2.0</h1>
-      <Charities />
+      <Charities/>
+      
 
     </div>
   );
--- src/components/charities.js
+++ src/components/charities.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { donate } from "/home/ashi_unix/newwild/frontend/src/components/SupportTokenWrapper.js";
+import { donate } from "./SupportTokenWrapper"; // Corrected import path
 
 const CharityCard = ({
   imageSrc,
@@ -7,32 +7,51 @@ const CharityCard = ({
   description,
   address,
   onDonate,
+  
 }) => {
   const [donationAmount, setDonationAmount] = useState("");
-  const [donationAddress, setDonationAddress] = useState("");
+  const [donated, setDonated] = useState(false);
+  const [connected, setConnected] = useState(false); // Add connected state
+
+  const connectToMetaMask = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        console.log('Connected to MetaMask!', accounts);
+        setConnected(true); // Set connected state to true
+      } else {
+        console.error('MetaMask not found. Please install MetaMask to use this application.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
   const handleDonationSubmit = (event) => {
     event.preventDefault();
-    onDonate(donationAmount, donationAddress);
+    onDonate(donationAmount);
     setDonationAmount("");
-    setDonationAddress("");
   };
 
   return (
     <div className="card">
+       <button
+        className="bg-blue-500 text-white rounded-md py-2 px-4 mt-4 hover:bg-blue-600"
+        onClick={connectToMetaMask}
+      >
+        Connect to MetaMask
+      </button>
       <img src={imageSrc} alt={name} />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text mt-4">{description}</p>
+        <p className="card-text mt-4">{address}</p>
         <form onSubmit={handleDonationSubmit}>
           <div className="form-group mt-4">
-            <label htmlFor="donationAmount">Enter Recipient Address: </label>
-            <input
-              type="text"
-              className="form-control"
-              id="donationAddress"
-              value={donationAddress}
-              onChange={(event) => setDonationAddress(event.target.value)}
-            />
             <label htmlFor="donationAmount" className="mt-4">
               Enter donation amount:{" "}
             </label>
@@ -62,38 +81,34 @@ const charities = [
     imageSrc:
       "https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hhcml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
   },
-  // ... More charities ...
+  {
+    name : "Hero",
+    description: "The National Marine Mammal Foundation, San Diego",
+    address : "0xee6E56276328b33C6250db0252125A8BaD0E38DE",
+    imageSrc:
+      "https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hhcml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  }
 ];
 
 const Charities = () => {
-  const [donated, setDonated] = useState(false);
-
-  const handleDonate = async (amount, address) => {
+  const handleDonate = async (amount) => {
     try {
-      console.log("got here");
-      try {
-        await donate(amount, address);
-        alert("donation successful");
-        console.log("here again");
-      } catch (error) {
-        console.error(error);
-      }
-      // Update the UI to show the donation was successful
-      setDonated(true);
+      await donate(amount);
+      alert("Donation successful!");
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-10">
+    
       <div className="flex flex-wrap -mx-4">
         {charities.map((charity, index) => (
           <div key={index} className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
             <CharityCard
               {...charity}
               onDonate={handleDonate}
-              donated={donated}
             />
           </div>
         ))}
@@ -103,3 +118,5 @@ const Charities = () => {
 };
 
 export default Charities;
+
+
